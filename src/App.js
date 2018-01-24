2ee9fecaed11a4ed5401a9498d6d3bbb70c0fbe4
Juan <juandavidtabaresarce@gmail.com>
2018-01-24 00:26:16 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to TravelDistance app test by Juan David Arce </h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          This is a test for <code>MOZIO</code>.
         </p>
       </div>
     );
