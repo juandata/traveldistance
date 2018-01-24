27951168769c19aa534ed80824ca5012e0c0f30e
Juan <juandavidtabaresarce@gmail.com>
2018-01-24 02:32:24 -0500

    updating

--- src/App.js
+++ src/App.js
@@ -19,3 +19,12 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+/*AIzaSyDn8ctUY9eDqqGCpEFt_vI858za3n2RsXk
+http://maps.googleapis.com/maps/api/distancematrix/outputFormat?parameters
+https://maps.googleapis.com/maps/api/distancematrix/json?origins=Manizales+CO&destinations=Medellin+CO&language=en&key=AIzaSyDn8ctUY9eDqqGCpEFt_vI858za3n2RsXk
+https://maps.googleapis.com/maps/api/distancematrix/xml?origins=Vancouver+BC|Seattle&destinations=San+Francisco|Vancouver+BC&mode=bicycling&language=fr-FR&key=YOUR_API_KEY
+
+*/
